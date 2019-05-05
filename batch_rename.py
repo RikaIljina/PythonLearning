cb7edcb90f45c3612f6d25c3fc3f75eaa8321d54
RikaIljina <wasilika@hotmail.com>
2019-05-06 00:12:00 +0200

    updated

--- batch_rename.py
+++ batch_rename.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 import re
 
+# abs_path = r'C:\Users\wasil\OneDrive\Desktop\Python\GitReps\PythonLearning\word_count_script'
+
 
 def make_file_list():
-    my_path = []
+    print('Enter the path to your files:')
+    abs_path = input()
+    my_paths = []
     choice = ''
     print('What files should be renamed?')
     print('1. Starts with...')
@@ -24,16 +28,17 @@ def make_file_list():
         ext_string = input()
         match_str = '.' + ext_string + '$'
 
-    for p in Path('.').iterdir():
+    for p in Path(abs_path).iterdir():
+        print(p)
         if p.is_file():
-            if re.search(match_str, str(p)):
+            if re.search(match_str, str(p.name)):
                 print(p)
-                my_path.append(str(p))
+                my_paths.append(str(p))
 
-    return my_path
+    return my_paths
 
 
-def add_prefix(my_path, new_stems=None):
+def add_prefix(my_paths, new_stems=None):
     choice = ''
     print('\n1. Add unified prefix\n2. Add sequential prefix')
     while choice != '1' and choice != '2':
@@ -42,20 +47,20 @@ def add_prefix(my_path, new_stems=None):
         print('\nEnter prefix:\n')
         pref = input()
         print('Renaming...')
-        for f in my_path:
+        for f in my_paths:
             p = Path(f)
-            p.rename(Path(p.parent, "{}_{}".format(pref, p.stem) + p.suffix))
+            p.rename(Path(p.parent, "{}{}".format(pref, p.stem) + p.suffix))
     elif choice == '2':
         print('\nEnter prefix (use $ as placeholder for number, e.g. file$$$ for file001, file002 etc):\n')
         pref = input()
         print('\nSequence starts with number:\n')
-        count = int(input())
+        sequence_incr = int(input())
         match = re.search(r'(\$+)', pref)
         print('Renaming...')
 
-        for f in my_path:
+        for f in my_paths:
             p = Path(f)
-            sequence = '0' * (len(match.group(0))-1) + str(count)
+            sequence = '0' * (len(match.group(0))-1) + str(sequence_incr)
             if len(sequence) > len(match.group(0)) and sequence[0] == '0':
                 sequence = sequence[1:]
             print(sequence)
@@ -65,35 +70,41 @@ def add_prefix(my_path, new_stems=None):
                 new_stem = p.stem
             else:
                 new_stem = new_stems[f]
-            p.rename(Path(p.parent, "{}_{}".format(pref, new_stem) + p.suffix))
-            count += 1
+            p.rename(Path(p.parent, "{}{}".format(pref, new_stem) + p.suffix))
+            sequence_incr += 1
     return
 
 
-def change_prefix(my_path):
+def change_prefix(my_paths):
     choice = ''
     new_stems = {}
-    print('\nWhich prefix do you want to remove?\n')
+    print('\nWhich prefix do you want to remove? (Use $ as wildcard)\n')
     pref = input()
-    for f in my_path:
+    for f in my_paths:
         p = Path(f)
-        pref = pref.replace('$','.')
-        match = re.search(pref, f)
+        pref = pref.replace('$', '.')
+        match = re.search(pref, str(p.name))
         print(pref, match)
-        print(p)
+        print(p.name)
         print(str(p.stem)[match.end():])
         new_stem = str(p.stem)[match.end():]
         new_stems[f] = new_stem
 
     print('\nThe remaining part of the filename is ', new_stem)
-    print('\nDo you want to replace it with a different prefix?\n')
+    print('\nDo you want to replace it with a different prefix?')
     print('\n1. Add different prefix\n2. Just remove old prefix')
     while choice != '1' and choice != '2':
         choice = input()
     if choice == '1':
-        add_prefix(my_path, new_stems)
+        new_paths = []
+        for f in my_paths:
+            p = Path(f)
+            p.rename(Path(p.parent, new_stems[f] + p.suffix))
+            new_paths.append(str(Path(p.parent, new_stems[f] + p.suffix)))
+            print(str(Path(p.parent, new_stems[f] + p.suffix)))
+        add_prefix(new_paths, new_stems)
     else:
-        for f in my_path:
+        for f in my_paths:
             p = Path(f)
             p.rename(Path(p.parent, new_stems[f] + p.suffix))
         print('\nFinished\n')
@@ -102,17 +113,17 @@ def change_prefix(my_path):
 
 
 def main():
-    my_path = make_file_list()
-    print('\nThese files will be renamed:\n', my_path)
+    my_paths = make_file_list()
+    print('\nThese files will be renamed:\n', my_paths)
     print('\nWhat do you want to do?\n1: Add prefix\n2: Change prefix\n')
     choice = 0
     while choice != '1' and choice != '2':
         choice = input()
 
     if choice == '1':
-        add_prefix(my_path)
+        add_prefix(my_paths)
     else:
-        change_prefix(my_path)
+        change_prefix(my_paths)
 
 
 if __name__ == "__main__":
--- menu_template.py
+++ menu_template.py
@@ -19,17 +19,12 @@ class Menu:
         }
         self.choices_lvl3 = {
             '1': add_prefix,
-            '2': change_prefix,
+            '2': remove_prefix,
             'q': self.run_lvl2
         }
         self.choices_lvl3_1 = {
             '1': unified_prefix,
             '2': sequential_prefix,
-            'q': self.run_lvl3
-        }
-        self.choices_lvl3_2 = {
-            '1': add_prefix,
-            '2': remove_prefix,
             'q': self.run_lvl3
         }
 
@@ -42,7 +37,7 @@ class Menu:
             else:
                 print('wrong input')
 
-    def run_lvl2(self, path_object):
+    def run_lvl2(self, path_object, *args):
         while True:
             print('1: List all files\n2: Select by prefix\n3. Select by content\n4. Select by extension\nq: Back')
             c = input()
@@ -51,30 +46,21 @@ class Menu:
             else:
                 print('wrong input')
 
-    def run_lvl3(self, path_object):
+    def run_lvl3(self, path_object, *args):
         while True:
-            print('1: Add prefix\n2: Change prefix\nq: Back')
+            print('1: Add prefix\n2: Remove prefix\nq: Back')
             c = input()
             if self.choices_lvl3.get(c):
-                self.choices_lvl3[c](path_object)
+                self.choices_lvl3[c](path_object, *args)
             else:
                 print('wrong input')
 
-    def run_lvl3_1(self, path_object):
+    def run_lvl3_1(self, path_object, *args):
         while True:
             print('1: Add unified prefix\n2: Add sequential prefix\nq: Back')
             c = input()
             if self.choices_lvl3_1.get(c):
                 self.choices_lvl3_1[c](path_object)
-            else:
-                print('wrong input')
-
-    def run_lvl3_2(self, path_object):
-        while True:
-            print('1: Add new prefix\n2: Just remove prefix\nq: Back')
-            c = input()
-            if self.choices_lvl3_2.get(c):
-                self.choices_lvl3_2[c](path_object)
             else:
                 print('wrong input')
 
@@ -86,8 +72,11 @@ class PathToRename:
         self.user_paths = []
         self.changed_paths = []
         self.stems = {}
+        self.allowed_chars = '^[a-zA-Z0-9\.\-_\$\s]+$'
 
 
+###################################################
+# Lets user enter a path
 def enter_path():
     # Asks user for path input
     # checks if path is valid
@@ -102,15 +91,21 @@ def enter_path():
             print('Bad path, try again')
     new_path_object = PathToRename(Path(user_path))      # remember path in object PathToRename
     menu_inst.run_lvl2(new_path_object)                  # open next submenu
+###################################################
 
 
+###################################################
+# Shows all files at the path chosen by user
 def list_all_files(path_object):
     print('path entered: ', path_object.path)
     for p in Path(path_object.path).iterdir():
         if p.is_file():
             print(p.name)
+###################################################
 
 
+###################################################
+# User chooses which files to rename in the directory
 def select_by_start(path_object):
     print('Enter start string:')
     start_string = input()
@@ -133,56 +128,139 @@ def select_by_ext(path_object):
     match_str = '[\.]' + ext_string + '$'
     path_object.matchstring = match_str
     make_selection(path_object, path_object.matchstring)
+###################################################
 
 
+###################################################
+# A list with all files to be renamed is created
 def make_selection(path_object, match):
     for p in Path(path_object.path).iterdir():
         if p.is_file():
             if re.search(match, str(p.name)):
                 print(p.name)
-                path_object.user_paths.append(str(p))
-    menu_inst.run_lvl3(path_object)                      # open next submenu
+                path_object.user_paths.append(str(p))       # full path to file is saved
+    menu_inst.run_lvl3(path_object)                         # open next submenu
+###################################################
 
 
-def add_prefix(path_object):
-    menu_inst.run_lvl3_1(path_object)
-
-
-def change_prefix(path_object):
-    print('changing prefix')
-    menu_inst.run_lvl3_2(path_object)
-
-
-def unified_prefix(path_objects):
-    print('unified prefix')
+###################################################
+# Asks user for new prefix
+def get_user_prefix(path_object):
     print('\nEnter prefix:\n')
-    pref = input()                          # TODO: check if this is a valid file name
+    pref = input()
+    reg = re.compile(path_object.allowed_chars)
+    print(reg.match(pref))
+    if reg.match(pref) and len(pref) <= 32:
+        return pref
+    else:
+        print('Invalid prefix: contains invalid characters or is longer than 32 characters')
+        return None
+###################################################
+
+
+###################################################
+# Calls submenu for adding a prefix
+def add_prefix(path_object, *args):
+    menu_inst.run_lvl3_1(path_object, *args)
+###################################################
+
+
+###################################################
+# Allows to select a prefix to change or remove
+# then call submenu
+def change_prefix(path_object):
+    print('\nEnter prefix to remove from file name (use $ or . as wildcard for one character):\n')
+    pref = input()
+
+    print(f'\nthe prefix {pref} will be removed.\nDo you want to replace the removed prefix with a different prefix?')
+    menu_inst.run_lvl3_2(path_object, pref)
+###################################################
+
+
+###################################################
+# Option from submenu "Add prefix": add same prefix
+# to all file names
+def unified_prefix(path_object):
+    pref = get_user_prefix(path_object)
+    if pref is None:
+        menu_inst.run_lvl3_1(path_object)
     print('Renaming...')
-    path_objects.changed_paths = []
-    for file in path_objects.user_paths:
+    path_object.changed_paths = []
+    for file in path_object.user_paths:
         p = Path(file)
-        if path_objects.stems.get(file) is None:
+        if path_object.stems.get(file) is None:
             new_stem = p.stem
         else:
-            new_stem = path_objects.stems[file]
+            new_stem = path_object.stems[file]
         p.rename(Path(p.parent, "{}{}".format(pref, new_stem) + p.suffix))
-        path_objects.changed_paths.append(str(Path(p.parent, "{}{}".format(pref, new_stem) + p.suffix)))
+        path_object.changed_paths.append(str(Path(p.parent, "{}{}".format(pref, new_stem) + p.suffix)))
 
-    path_objects.user_paths = path_objects.changed_paths[:]
-    print(path_objects.user_paths)
-
-
-def sequential_prefix(path_objects):
-    print('seq prefix')
+    path_object.user_paths = path_object.changed_paths[:]
+    print(path_object.user_paths)
+    return
+###################################################
 
 
-def remove_prefix(path_objects):
-    print('removing prefix')
+###################################################
+# Option from submenu "Add prefix": add a prefix
+# with a running sequence
+def sequential_prefix(path_object):
+    print('\nEnter prefix (use $ as placeholder for number, e.g. file$$$ for file001, file002 etc):')
+    pref = get_user_prefix(path_object)
+    print('\nSequence starts with number:')
+    sequence_incr = int(input())
+    match = re.search(r'(\$+)', pref)
+    print('Renaming...')
+
+    for f in path_object.user_paths:
+        p = Path(f)
+        sequence = '0' * (len(match.group(0)) - len(str(sequence_incr))) + str(sequence_incr)
+        if len(sequence) > len(match.group(0)) and sequence[0] == '0':
+            sequence = sequence[1:]
+        print(sequence)
+        pref = pref[:match.start()] + sequence + pref[match.end():]
+        print(pref)
+        p.rename(Path(p.parent, "{}{}".format(pref, p.stem) + p.suffix))
+        sequence_incr += 1
+        path_object.changed_paths.append(str(Path(p.parent, "{}{}".format(pref, p.stem) + p.suffix)))
+
+    path_object.user_paths = path_object.changed_paths[:]
+###################################################
 
 
+###################################################
+# Removes prefix from file name
+def remove_prefix(path_object):
+    print('\nEnter prefix to remove from file name (use $ or . as wildcard for one character):\n')
+    pref = input()
+
+    for f in path_object.user_paths:
+        p = Path(f)
+        pref = pref.replace('$', '.')
+        match = re.search(pref, str(p.name))
+        if not match:
+            path_object.changed_paths.append(str(p))
+            continue
+        new_stem = str(p.stem)[match.end():]
+        print('New file name: ', new_stem)
+        p.rename(Path(p.parent, new_stem + p.suffix))
+        path_object.changed_paths.append(str(Path(p.parent, new_stem + p.suffix)))
+
+    path_object.user_paths = path_object.changed_paths[:]
+
+    print(f'\nThe prefix {pref} was removed.\nDo you want to replace the removed prefix with a different prefix?')
+    menu_inst.run_lvl3(path_object)
+
+    return
+###################################################
+
+
+###################################################
+# Ends script execution
 def exit_menu(*args):
     print('Thank you for using this script.')
     sys.exit(0)
+###################################################
 
 
 if __name__ == '__main__':
